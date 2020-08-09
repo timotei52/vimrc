1f821b8f10b7f474ed7c4cd0cd32b63175b032ff
timotei52 <49104196+timotei52@users.noreply.github.com>
2020-08-10 00:02:01 +0300

    Update install_awesome_vimrc.sh

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -9,10 +9,10 @@ source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
-
+source ~/.vim_runtime/vimrcs/CompProgfile.vim
 try
 source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
-echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+echo "[*]"
